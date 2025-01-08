09ce49935fd6179a90f8d1e94a53ad3a6c7ccef6
Isma Navarro <ismanapa@gmail.com>
2025-01-08 17:35:38 +0100

    feat: Add basic enemy

---
+++ Scripts/enemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+enum Direction {
+	RIGHT, 
+	LEFT
+}
+
+@export var start_direction: Direction
+
+var max_speed: int = 60
+var direction: Vector2 = Vector2.RIGHT
+var gravity: int = 500
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	direction = Vector2.RIGHT if start_direction == Direction.RIGHT else Vector2.LEFT
+
+func _process(delta: float) -> void:
+	velocity.x = (direction * max_speed).x
+	
+	velocity.y +=  gravity * delta
+	
+	move_and_slide()
+	flip_enemy()
+	
+func flip_enemy():
+	anim.flip_h = direction.x > 0
+
+
+func _on_goal_detector_area_entered(area: Area2D) -> void:
+	direction *= -1
